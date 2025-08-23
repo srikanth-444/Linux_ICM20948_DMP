234f11957dd800e5333744a5ead24c2a43632562
srikanth-444 <srikanth.popuri4444@gmail.com>
2025-08-23 10:43:07 -0600

    full working library with circular buffer implementation

---
+++ ICM20948/build/CMakeFiles/PI_icm20948.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/src/utils/CircularBuffer.c" "CMakeFiles/PI_icm20948.dir/src/utils/CircularBuffer.c.o" "gcc" "CMakeFiles/PI_icm20948.dir/src/utils/CircularBuffer.c.o.d"
+  "/home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/src/utils/DataConverter.c" "CMakeFiles/PI_icm20948.dir/src/utils/DataConverter.c.o" "gcc" "CMakeFiles/PI_icm20948.dir/src/utils/DataConverter.c.o.d"
+  "/home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/src/utils/Icm20948Augmented.c" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948Augmented.c.o" "gcc" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948Augmented.c.o.d"
+  "/home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/src/utils/Icm20948AuxCompassAkm.c" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948AuxCompassAkm.c.o" "gcc" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948AuxCompassAkm.c.o.d"
+  "/home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/src/utils/Icm20948AuxTransport.c" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948AuxTransport.c.o" "gcc" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948AuxTransport.c.o.d"
+  "/home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/src/utils/Icm20948DataBaseControl.c" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948DataBaseControl.c.o" "gcc" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948DataBaseControl.c.o.d"
+  "/home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/src/utils/Icm20948DataBaseDriver.c" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948DataBaseDriver.c.o" "gcc" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948DataBaseDriver.c.o.d"
+  "/home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/src/utils/Icm20948DataConverter.c" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948DataConverter.c.o" "gcc" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948DataConverter.c.o.d"
+  "/home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/src/utils/Icm20948Dmp3Driver.c" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948Dmp3Driver.c.o" "gcc" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948Dmp3Driver.c.o.d"
+  "/home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/src/utils/Icm20948LoadFirmware.c" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948LoadFirmware.c.o" "gcc" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948LoadFirmware.c.o.d"
+  "/home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/src/utils/Icm20948MPUFifoControl.c" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948MPUFifoControl.c.o" "gcc" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948MPUFifoControl.c.o.d"
+  "/home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/src/utils/Icm20948SelfTest.c" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948SelfTest.c.o" "gcc" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948SelfTest.c.o.d"
+  "/home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/src/utils/Icm20948Setup.c" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948Setup.c.o" "gcc" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948Setup.c.o.d"
+  "/home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/src/utils/Icm20948Transport.c" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948Transport.c.o" "gcc" "CMakeFiles/PI_icm20948.dir/src/utils/Icm20948Transport.c.o.d"
+  "/home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/src/utils/Message.c" "CMakeFiles/PI_icm20948.dir/src/utils/Message.c.o" "gcc" "CMakeFiles/PI_icm20948.dir/src/utils/Message.c.o.d"
+  "/home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/src/Pi-ICM20948.cpp" "CMakeFiles/PI_icm20948.dir/src/Pi-ICM20948.cpp.o" "gcc" "CMakeFiles/PI_icm20948.dir/src/Pi-ICM20948.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ICM20948/build/CMakeFiles/PI_icm20948.dir/src/utils/Icm20948DataConverter.c.o.d
@@ -0,0 +1,43 @@
+CMakeFiles/PI_icm20948.dir/src/utils/Icm20948DataConverter.c.o: \
+ /home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/src/utils/Icm20948DataConverter.c \
+ /usr/include/stdc-predef.h \
+ /home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/include/Icm20948.h \
+ /home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/include/InvExport.h \
+ /home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/include/InvBool.h \
+ /home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/include/InvError.h \
+ /home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/include/Icm20948Setup.h \
+ /usr/lib/gcc/aarch64-linux-gnu/11/include/stdint.h /usr/include/stdint.h \
+ /usr/include/aarch64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/aarch64-linux-gnu/bits/wordsize.h \
+ /usr/include/aarch64-linux-gnu/bits/timesize.h \
+ /usr/include/aarch64-linux-gnu/sys/cdefs.h \
+ /usr/include/aarch64-linux-gnu/bits/long-double.h \
+ /usr/include/aarch64-linux-gnu/gnu/stubs.h \
+ /usr/include/aarch64-linux-gnu/gnu/stubs-lp64.h \
+ /usr/include/aarch64-linux-gnu/bits/types.h \
+ /usr/include/aarch64-linux-gnu/bits/typesizes.h \
+ /usr/include/aarch64-linux-gnu/bits/time64.h \
+ /usr/include/aarch64-linux-gnu/bits/wchar.h \
+ /usr/include/aarch64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/aarch64-linux-gnu/bits/stdint-uintn.h \
+ /home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/include/Icm20948Serif.h \
+ /usr/include/assert.h \
+ /home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/include/Icm20948Transport.h \
+ /home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/include/Icm20948DataConverter.h \
+ /home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/include/Icm20948AuxCompassAkm.h \
+ /home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/include/Icm20948SelfTest.h \
+ /usr/include/string.h /usr/lib/gcc/aarch64-linux-gnu/11/include/stddef.h \
+ /usr/include/aarch64-linux-gnu/bits/types/locale_t.h \
+ /usr/include/aarch64-linux-gnu/bits/types/__locale_t.h \
+ /usr/include/strings.h \
+ /home/raspi/spi/PI_ICM20948_DMP_Full-Function/ICM20948/include/Icm20948DataConverter.h \
+ /usr/include/math.h /usr/include/aarch64-linux-gnu/bits/math-vector.h \
+ /usr/include/aarch64-linux-gnu/bits/libm-simd-decl-stubs.h \
+ /usr/include/aarch64-linux-gnu/bits/floatn.h \
+ /usr/include/aarch64-linux-gnu/bits/floatn-common.h \
+ /usr/include/aarch64-linux-gnu/bits/flt-eval-method.h \
+ /usr/include/aarch64-linux-gnu/bits/fp-logb.h \
+ /usr/include/aarch64-linux-gnu/bits/fp-fast.h \
+ /usr/include/aarch64-linux-gnu/bits/mathcalls-helper-functions.h \
+ /usr/include/aarch64-linux-gnu/bits/mathcalls.h
